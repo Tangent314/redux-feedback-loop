76891f1661a1a54fc23d27741614481ed1ecc45c
Blair McLaughlin <blairmclaughlin@Blairs-MacBook-Pro.local>
2019-11-03 17:04:01 -0600

    make routes to views

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './App.css';
+import { connect } from 'react-redux'
+import { HashRouter as Router, Route } from 'react-router-dom'
+
+import Feeling from '../Feeling/Feeling'
+import Understanding from '../Understanding/Understanding';
+import Support from '../Support/Support';
+import Comments from '../Comments/Comments';
+import Review from '../Review/Review';
+import Success from '../Success/Success';
 
 class App extends Component {
   render() {
@@ -11,6 +19,15 @@ class App extends Component {
           <h4><i>Don't forget it!!!</i></h4>
         </header>
         <br/>
+        <Router>
+          <Route exact path="/" component={Feeling} />
+          <Route  path="/understanding" component={Understanding} />
+          <Route  path="/support" component={Support} />
+          <Route  path="/comments" component={Comments} />
+          <Route  path="/review" component={Review} />
+          <Route path="/success" component={Success}/>
+        </Router>
+          
       </div>
     );
   }
