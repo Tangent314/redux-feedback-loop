7313dbf84e30055a4ce89c34b9080f39b9189a32
Blair McLaughlin <blairmclaughlin@Blairs-MacBook-Pro.local>
2019-11-04 08:55:09 -0600

    remove two lines of code

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,8 +5,7 @@ import CardContent from '@material-ui/core/CardContent'
 import Input from '@material-ui/core/Input'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
-import swal from 'sweetalert';
-import axios from 'axios';
+
 
 
 class Comments extends Component {
